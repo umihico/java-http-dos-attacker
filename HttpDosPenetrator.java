fd543f8d252ed89b9472d739cb5cf7deb9057672
umihico <umihico@users.noreply.github.com>
2019-01-03 19:09:52 +0900

    fix bug

--- HttpDosPenetrator.java
+++ HttpDosPenetrator.java
@@ -6,11 +6,12 @@ import java.util.List;
 import java.util.concurrent.Future;
 // javac HttpDosPenetrator.java & java HttpDosPenetrator 30 https://uy34uokjte.execute-api.us-west-2.amazonaws.com/default/java-http-attacker-test-site
 public class HttpDosPenetrator {
-        public static void main(String[] args){
+        public static void main(String[] args) throws Exception {
                 Integer attackSeconds=Integer.parseInt(args[0]);
                 String targetUrl=args[1];
+                start(attackSeconds,targetUrl);
         }
-        public void start(Integer attackSeconds,String targetUrl) throws Exception {
+        public static void start(Integer attackSeconds,String targetUrl) throws Exception {
                 HttpParralelExecutor httpParralelExecutor = new HttpParralelExecutor(attackSeconds,targetUrl);
                 ResultPrinter resultPrinter=new ResultPrinter(attackSeconds,targetUrl,httpParralelExecutor.getDoneRequestQueue());
                 Future<Long> futureRequestedNum=httpParralelExecutor.startInBackground();
